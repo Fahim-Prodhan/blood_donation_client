581a31c71a2f166fb35617dd75ce3bfe7f10f90a
Fahim-Prodhan <prodhan15-4809@diu.edu.bd>
2024-06-05 05:06:58 +0600

    Dashboard for admin design

--- src/Pages/dashboardContent/DashboardContent.jsx
+++ src/Pages/dashboardContent/DashboardContent.jsx
@@ -1,9 +1,11 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../provider/AuthProvider';
 import useDonationRequest from '../../hook/useDonationRequest';
-import { FaEdit } from 'react-icons/fa';
+import { FaArrowDown, FaArrowUp, FaEdit, FaUsers } from 'react-icons/fa';
 import { MdDelete } from 'react-icons/md';
 import { Link } from 'react-router-dom';
+import { AiFillDollarCircle } from "react-icons/ai";
+import { BiSolidDonateHeart } from "react-icons/bi";
 
 const DashboardContent = () => {
     const { user } = useContext(AuthContext)
@@ -48,6 +50,46 @@ const DashboardContent = () => {
                 </div>
                 <div className='mt-4 text-center'>
                     <Link to='/dashboard/my-donation-requests'> <button className="btn bg-[#279eff53] text-[#279EFF]">view my all request</button></Link>
+                </div>
+            </div>
+
+            {/* For admin */}
+            <div className='grid grid-cols-1 lg:grid-cols-3 mx-4 gap-6 justify-center'>
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <div className="card-body">
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-square btn-md">
+                                <FaUsers className='text-2xl'  />
+                            </button>
+                        </div>
+                        <p>Total User</p>
+                        <h1 className='text-2xl font-semibold flex items-center gap-4'>4500 <span className='flex items-center text-xs px-2 py-1 bg-[#74e29140] text-[#74E291] rounded-md'><FaArrowUp /> +5.9%</span></h1>
+                        <p className='flex items-center'>increase by <span className='flex items-center px-2 py-1 text-[#74E291] rounded-md'><FaArrowUp className='text-xs' /> +5.9%</span> this month</p>
+                    </div>
+                </div>
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <div className="card-body">
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-square btn-md">
+                            <AiFillDollarCircle className='text-2xl' />
+                            </button>
+                        </div>
+                        <p>Total funding</p>
+                        <h1 className='text-2xl font-semibold flex items-center gap-4'>$4500 <span className='flex items-center text-xs px-2 py-1 bg-[#ff204d27] text-[#FF204E] rounded-md'><FaArrowDown /> +5.9%</span></h1>
+                        <p className='flex items-center'>decrease by <span className='flex items-center px-2 py-1 text-[#FF204E] rounded-md'><FaArrowDown className='text-xs' /> +5.9%</span> this month</p>
+                    </div>
+                </div>
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <div className="card-body">
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-square btn-md">
+                            <BiSolidDonateHeart className='text-2xl' />
+                            </button>
+                        </div>
+                        <p>Total Blood Donation Request</p>
+                        <h1 className='text-2xl font-semibold flex items-center gap-4'>4500 <span className='flex items-center text-xs px-2 py-1 bg-[#74e29140] text-[#74E291] rounded-md'><FaArrowUp /> +5.9%</span></h1>
+                        <p className='flex items-center'>increase by <span className='flex items-center px-2 py-1 text-[#74E291] rounded-md'><FaArrowUp className='text-xs' /> +5.9%</span> this month</p>
+                    </div>
                 </div>
             </div>
         </div>
