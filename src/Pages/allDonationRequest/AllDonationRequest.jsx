0f2d067c7bc26e3a390f739ad3ec237514c8f9cc
Fahim-Prodhan <prodhan15-4809@diu.edu.bd>
2024-06-08 03:49:10 +0600

    Delete my-donation-requested apply

--- src/Pages/allDonationRequest/AllDonationRequest.jsx
+++ src/Pages/allDonationRequest/AllDonationRequest.jsx
@@ -3,19 +3,54 @@ import React from 'react';
 import useAxiosSecure from '../../hook/useAxiosSecure';
 import { MdDelete } from 'react-icons/md';
 import { FaEdit } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const AllDonationRequest = () => {
 
     const axiosSecure = useAxiosSecure()
 
-    const {data: allDonationRequest = []} = useQuery({
+    const {refetch,data: allDonationRequest = []} = useQuery({
         queryKey:['allDonationReq'],
         queryFn: async ()=>{
             const res = await axiosSecure.get('/all-blood-donation-request')
             return res.data
         }
-
     })
+
+    const handleDeleteDonationReq = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/my-donation-request/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                            refetch()
+                        } else {
+                            Swal.fire({
+                                icon: "error",
+                                title: "Oops...",
+                                text: "Something went wrong!"
+                            });
+                        }
+                    })
+
+            }
+        });
+
+    }
     
     return (
         <div>
@@ -46,7 +81,7 @@ const AllDonationRequest = () => {
                                         <td>{d.donationTime}</td>
                                         <td>{d.status}</td>
                                         <td>{d?.donarInformation}</td>
-                                        <td className='space-x-2'><button className='text-[#615EFC] text-2xl'><FaEdit /></button> <button className='text-[#FF204E] text-2xl'><MdDelete /></button></td>
+                                        <td className='space-x-2'><Link to={`/dashboard/update-donation-requests/${d._id}`}><button className='text-[#615EFC] text-2xl'><FaEdit /></button> </Link> <button onClick={()=>handleDeleteDonationReq(d._id)} className='text-[#FF204E] text-2xl'><MdDelete /></button></td>
                                         <td><button className='text-[#41B06E] hover:bg-[#41b06d5c] px-2 py-1 rounded-md '>view</button></td>
                                     </tr>
                                     )
--- src/Pages/myDonationRequests/MyDonationRequests.jsx
+++ src/Pages/myDonationRequests/MyDonationRequests.jsx
@@ -3,10 +3,47 @@ import useDonationRequest from '../../hook/useDonationRequest';
 import { FaEdit } from 'react-icons/fa';
 import { MdDelete } from 'react-icons/md';
 import { Link } from 'react-router-dom';
+import useAxiosSecure from '../../hook/useAxiosSecure';
+import Swal from 'sweetalert2';
 
 const MyDonationRequests = () => {
 
-    const {donationRequests} = useDonationRequest()
+    const { donationRequests,refetch } = useDonationRequest()
+    const axiosSecure = useAxiosSecure();
+
+    const handleDeleteDonationReq = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/my-donation-request/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                            refetch()
+                        } else {
+                            Swal.fire({
+                                icon: "error",
+                                title: "Oops...",
+                                text: "Something went wrong!"
+                            });
+                        }
+                    })
+
+            }
+        });
+
+    }
 
 
     return (
@@ -30,7 +67,7 @@ const MyDonationRequests = () => {
                         </thead>
                         <tbody>
                             {
-                                donationRequests.map(d=>
+                                donationRequests.map(d =>
                                     <tr key={d._id}>
                                         <td>{d.recipientName}</td>
                                         <td>{d.upazila}, {d.district}</td>
@@ -38,10 +75,10 @@ const MyDonationRequests = () => {
                                         <td>{d.donationTime}</td>
                                         <td>{d.status}</td>
                                         <td>{d?.donarInformation}</td>
-                                        <td className='space-x-2'><Link to={`/dashboard/update-donation-requests/${d._id}`}><button className='text-[#615EFC] text-2xl'><FaEdit /></button> </Link><button className='text-[#FF204E] text-2xl'><MdDelete /></button></td>
+                                        <td className='space-x-2'><Link to={`/dashboard/update-donation-requests/${d._id}`}><button className='text-[#615EFC] text-2xl'><FaEdit /></button> </Link><button onClick={()=>handleDeleteDonationReq(d._id)} className='text-[#FF204E] text-2xl'><MdDelete /></button></td>
                                         <td><button className='text-[#41B06E] hover:bg-[#41b06d5c] px-2 py-1 rounded-md '>view</button></td>
                                     </tr>
-                                    )
+                                )
                             }
                         </tbody>
                     </table>
