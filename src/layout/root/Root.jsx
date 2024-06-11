903e88bec40bc4cc43f50bff43a45bb1152a0180
Fahim-Prodhan <prodhan15-4809@diu.edu.bd>
2024-06-11 16:53:34 +0600

    Footer add

--- src/layout/root/Root.jsx
+++ src/layout/root/Root.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../../components/navbar/Navbar';
+import Footer from '../../components/footer/Footer';
 
 const Root = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <div className='mt-12'><Footer></Footer></div>
         </div>
     );
 };
